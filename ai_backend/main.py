32196251b761d09a739b562a3e53e688a3d695a3
정진영 <wlsehfdl95@naver.com>
2024-03-06 17:31:51 +0900

    [AI] Feat: Fast API 함수 작성
    Former-commit-id: 4391a6368d763c6d1a6e35cf16ddeea5b2389cc3

--- ai_backend/main.py
+++ ai_backend/main.py
@@ -1,20 +1,35 @@
-from typing import Union
-from fastapi import FastAPI
-import numpy as np
-import os
-import glob
-import cv2
-import matplotlib.pyplot as plt
+# fastapi
+from fastapi import FastAPI, UploadFile
+from starlette.responses import FileResponse
 
+# insightface
 import insightface
 from insightface.app import FaceAnalysis
-from insightface.data import get_image as ins_get_image
+import matplotlib.pyplot as plt
+
 
 app = FastAPI()
+
+# face swap app
+faceswap_app = FaceAnalysis(name="buffalo_l")
+faceswap_app.prepare(ctx_id=0, det_size=(640, 640))
+
+# swapper model
+swapper_path = "./inswapper_128.onnx"
+swapper = insightface.model_zoo.get_model(
+    swapper_path, download=False, download_zip=False
+)
+
+# img dir
+bg_0_img_path = "./bg_0.jpg"
+bg_0_img = plt.imread(bg_0_img_path)
 
 # to run the server
 # uvicorn main:app --reload
 
+######################################################################
+######################################################################
+# 함수 예시
 # @app.get("/")
 # def read_root():
 #     return {"Hello": "World"}
@@ -23,3 +38,20 @@ app = FastAPI()
 # @app.get("/items/{item_id}")
 # def read_item(item_id: int, q: Union[str, None] = None):
 #     return {"item_id": item_id, "q": q}
+######################################################################
+######################################################################
+
+
+@app.post("/")
+def process_image(userImage: UploadFile):
+    # bg_faces = faceswap_app.get(bg_0_img)
+    # bg_face = bg_faces[0]
+
+    # faces = faceswap_app.get(userImage)
+    # source_face = faces[0]
+
+    # processed_image = bg_0_img.copy()
+    # processed_image = swapper.get(bg_0_img, bg_face, source_face, paste_back=True)
+
+    # return {"processedImage": processed_image}
+    return {"userImage": userImage}  # 이미지 정보 확인 디버깅용
