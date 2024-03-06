be9a218c8ea1944ff4064470c1f189ebf452ce44
정진영 <wlsehfdl95@naver.com>
2024-03-06 10:17:28 +0900

    [AI] Feat: init

--- ai_backend/main.py
+++ ai_backend/main.py
@@ -1,5 +1,14 @@
 from typing import Union
 from fastapi import FastAPI
+import numpy as np
+import os
+import glob
+import cv2
+import matplotlib.pyplot as plt
+
+import insightface
+from insightface.app import FaceAnalysis
+from insightface.data import get_image as ins_get_image
 
 app = FastAPI()
 
