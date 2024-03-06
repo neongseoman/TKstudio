97740a17b2a72e781015f5306bfa5ad5f9f7dc54
고정원 <7704wjddnjs@naver.com>
2024-03-06 17:59:20 +0900

    [BE] Feat: Spring Security에 JWT 필터 적용1

--- backend/src/main/java/com/ssafy/gallery/config/SecurityConfig.java
+++ backend/src/main/java/com/ssafy/gallery/config/SecurityConfig.java
@@ -1,50 +1,58 @@
 package com.ssafy.gallery.config;
 
-import com.ssafy.gallery.user.service.OAuth2Service;
+import com.ssafy.gallery.config.jwt.JwtAuthenticationFilter;
+import com.ssafy.gallery.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
 
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private CorsConfig corsConfig;
+
     private static final String[] AUTH_WHITELIST = {
-            "/api/**", "/graphiql", "/graphql",
-            "/swagger-ui/**", "/api-docs", "/swagger-ui-custom.html",
-            "/v3/api-docs/**", "/api-docs/**", "/swagger-ui.html"
+            "/api/v1/**", "/swagger-ui/**", "/api-docs", "/swagger-ui-custom.html",
+            "/api-docs/**", "/swagger-ui.html"
     };
 
-    private final OAuth2Service oAuth2Service;
-
-    public SecurityConfig(OAuth2Service oAuth2Service) {
-        this.oAuth2Service = oAuth2Service;
-    }
-
+    // 특정 HTTP 요청에 대한 웹 기반 보안 구성
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
-        return httpSecurity
-                .authorizeHttpRequests(
-                        authorize -> authorize
-                                .shouldFilterAllDispatcherTypes(false)
-                                .requestMatchers(AUTH_WHITELIST)
-                                .permitAll()
-                                .anyRequest()
-                                .authenticated()
-                )
-                .httpBasic().disable()
-                .formLogin().disable()  // 폼 로그인 사용 X
-                .logout().disable() // 로그아웃 사용 X
-                .cors().disable()
-                .csrf().disable() // csrf 보안 설정 사용 X
-                .oauth2Login() // OAuth2를 통한 로그인 사용
-                .defaultSuccessUrl("/api/v1/user/loginInfo", true) // 로그인 성공시 이동할 URL
-                .userInfoEndpoint() // 사용자가 로그인에 성공하였을 경우,
-                .userService(oAuth2Service) // 해당 서비스 로직을 타도록 설정
-                .and().and()
-                .build();
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        AuthenticationManager authenticationManager = http.getSharedObject(AuthenticationManager.class);
+
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .addFilter(corsConfig.corsFilter())
+                .addFilter(new JwtAuthenticationFilter(authenticationManager)) // AuthenticationManger
+
+//                .cors(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .formLogin(AbstractHttpConfigurer::disable)
+//                .logout(AbstractHttpConfigurer::disable)
+                .logout((logout) -> logout
+                        .logoutSuccessUrl("/login")
+                        .invalidateHttpSession(true))
+                .authorizeHttpRequests((authorize) -> authorize
+                        .requestMatchers(AUTH_WHITELIST).permitAll()
+//                        .requestMatchers("/index.html", "/feed/**", "/albums/**", "/photo/**", "/user/signup", "/", "/login", "/album/init").permitAll()
+                        .anyRequest().authenticated())
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                );
+        return http.build();
     }
 }
---
+++ backend/src/main/java/com/ssafy/gallery/jwt/filter/MyFilter1.java
@@ -0,0 +1,15 @@
+package com.ssafy.gallery.jwt.filter;
+
+
+import jakarta.servlet.*;
+
+import java.io.IOException;
+
+public class MyFilter1 implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        System.out.println("필터1");
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+}
