ca0ecac36721d2ff790d7bbad74e9d55f7f834cd
박근수 <rmstn8426@gmail.com>
2024-03-06 13:25:06 +0900

    [BE] Feat: return ResonseEntity<Processed img>, Image Service layer

--- backend/src/main/java/com/ssafy/gallery/image/controller/ImageController.java
+++ backend/src/main/java/com/ssafy/gallery/image/controller/ImageController.java
@@ -1,29 +1,50 @@
 package com.ssafy.gallery.image.controller;
 
-
-import com.ssafy.gallery.example.controller.ExampleApiController;
+import com.ssafy.gallery.image.model.ImageOption;
+import com.ssafy.gallery.image.service.ImageService;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.apache.commons.io.IOUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
 
 @Tag(name = "이미지", description = "Image API")
 @Log4j2
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/api/v1/image")
 public class ImageController {
     private static final Logger logger = LogManager.getLogger(ImageController.class);
+    private final ResourceLoader resourceLoader;
+    private final ImageService imageService;
 
+    // Login한 사용자의 UserId값이 있어야 JPA로 DB에 데이터 넘길 수 있음.
     @PostMapping("/create")
-    public String createImage(
-            @RequestParam("image") MultipartFile image
-            ){
-            logger.info(image.getContentType());
-            logger.info(image.getOriginalFilename());
-            logger.info(image.getSize());
+    public ResponseEntity<Resource> createImage(
+            @RequestParam(value = "image") MultipartFile image,
+            @RequestParam(value = "background") String background,
+            @RequestParam(value = "suit") String suit,
+            @RequestParam(value = "hair") String hair
+    ) throws IOException {
 
-            return "image income";
+        imageService.sendImageToFastAPI(image,new ImageOption(background,suit,hair));
+
+        Resource resource = image.getResource();
+
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_TYPE, MediaType.IMAGE_JPEG_VALUE)
+                .body(resource);
     }
 }
