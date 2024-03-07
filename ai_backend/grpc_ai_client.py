44ffdcea679662ce2da435dd5cead4a59e45a3e1
정진영 <wlsehfdl95@naver.com>
2024-03-07 15:45:24 +0900

    [AI] Feat: gRPC test
    Former-commit-id: 8e476cbc9e99639ad225701de4cc900b2788a354

--- ai_backend/grpc_ai_client.py
+++ ai_backend/grpc_ai_client.py
@@ -3,7 +3,7 @@ import ddalkkak_pb2_grpc as pb2_grpc
 import ddalkkak_pb2 as pb2
 
 
-class Ddalkkak(object):
+class DdalkkakClient(object):
     def __init__(self):
         self.host = "localhost"
         self.server_port = 50051
@@ -16,11 +16,11 @@ class Ddalkkak(object):
 
     def get_url(self, name):
         name = pb2.TestRequest(name=name)
-        # print(f"{name}")
+        print(f"{name}")
         return self.stub.TestFunc(name)
 
 
 if __name__ == "__main__":
-    client = Ddalkkak()
+    client = DdalkkakClient()
     result = client.get_url(name="박근수")
     print(f"{result}")
