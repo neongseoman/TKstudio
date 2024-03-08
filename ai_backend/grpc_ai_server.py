a692eeb86536f27f7a284b2acfe87fa181388554
정진영 <wlsehfdl95@naver.com>
2024-03-08 17:40:19 +0900

    [AI] Feat: grpc receive image test, not done yet
    Former-commit-id: afa762893b46b0d8b7c439e5a2d8e0be47354080

--- ai_backend/grpc_ai_server.py
+++ ai_backend/grpc_ai_server.py
@@ -1,9 +1,27 @@
 import grpc
 from concurrent import futures
-import time
 
 import image_pb2 as pb2
 import image_pb2_grpc as pb2_grpc
+
+# insightface
+import insightface
+from insightface.app import FaceAnalysis
+import matplotlib.pyplot as plt
+
+# face swap app
+faceswap_app = FaceAnalysis(name="buffalo_l")
+faceswap_app.prepare(ctx_id=0, det_size=(640, 640))
+
+# swapper model
+swapper_path = "./inswapper_128.onnx"
+swapper = insightface.model_zoo.get_model(
+    swapper_path, download=False, download_zip=False
+)
+
+# img dir
+bg_0_img_path = "./bg_0.jpg"
+bg_0_img = plt.imread(bg_0_img_path)
 
 
 class CreateImageService(pb2_grpc.CreateImageServicer):
@@ -14,10 +32,27 @@ class CreateImageService(pb2_grpc.CreateImageServicer):
     def sendImage(self, request, context):
 
         original_image = request.originalImage
+        # bytes to ndarray
+        # original_image = np.frombuffer(original_image_bytes, dtype=np.uint8)
         # options = request.options
 
-        # 이미지 가공하는 코드
+        ############################################################
+        # # detect face from bg img
+        # bg_faces = faceswap_app.get(bg_0_img)
+        # bg_face = bg_faces[0]
+
+        # # detect face from input img
+        # faces = faceswap_app.get(original_image)
+        # source_face = faces[0]
+
+        # # swap face from bg img to input img
+        # processed_image = bg_0_img.copy()
+        # processed_image = swapper.get(bg_0_img, bg_face, source_face, paste_back=True)
+        ############################################################
+
+        # ndarray to bytes
         processed_image = original_image
+        # processed_image = processed_image.tobytes()
 
         # S3에 이미지 저장하는 코드
 
@@ -40,9 +75,13 @@ def serve():
     pb2_grpc.add_CreateImageServicer_to_server(CreateImageService(), server)
     server.add_insecure_port("[::]:50051")
     print("server port is", 50051)
+    server.start()
     print("grpc server is now running")
     print("if you want to quit the grpc server, press 'ctrl + C'")
-    server.start()
+    print()
+    print()
+    print()
+    print()
     server.wait_for_termination()
 
 
