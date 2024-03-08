051bd182f2c15e425f842251e2ae8dd093dfed11
정진영 <wlsehfdl95@naver.com>
2024-03-08 14:53:08 +0900

    [AI] Feat: proto 수정 및 테스트
    Former-commit-id: 832961ae67419a89a53becb419f65e2010166d1b

--- ai_backend/grpc_ai_client.py
+++ ai_backend/grpc_ai_client.py
@@ -1,9 +1,12 @@
 import grpc
-import ddalkkak_pb2 as pb2
-import ddalkkak_pb2_grpc as pb2_grpc
+
+import image_pb2 as pb2
+import image_pb2_grpc as pb2_grpc
+
+import matplotlib.pyplot as plt
 
 
-class DdalkkakClient(object):
+class TestClient(object):
     def __init__(self):
         self.host = "localhost"
         self.server_port = 50051
@@ -12,16 +15,31 @@ class DdalkkakClient(object):
             "{}:{}".format(self.host, self.server_port)
         )
 
-        self.stub = pb2_grpc.DdalkkakStub(self.channel)
+        self.stub = pb2_grpc.CreateImageStub(self.channel)
 
-    def inputImage(self, userImage, options):
-        ddalkkakrequest = pb2.DdalkkakRequest(userImage=userImage, options=options)
+    def inputImage(self, originalImage, options):
+        OriginalImageInfo = pb2.OriginalImageInfo(
+            originalImage=originalImage, options=options
+        )
 
-        return self.stub.ProcessImage(ddalkkakrequest)
+        return self.stub.sendImage(OriginalImageInfo)
 
 
 if __name__ == "__main__":
-    client = DdalkkakClient()
-    test_byte = b"Hello, world!"
-    result = client.inputImage(userImage=test_byte, options="myOptions")
-    print(f"{result}")
+    client = TestClient()
+    original_image_path = "./rdj.jpg"
+    original_image = plt.imread(original_image_path)
+    original_image = original_image.tobytes()
+    options = {
+        "background": "0",
+        "suit": "0",
+        "hair": "0",
+    }
+
+    result = client.inputImage(originalImage=original_image, options=options)
+    # print(f"result:\n{result}")
+    print("processedImage:")
+    print(result.processedImage)
+    print()
+    print("responseUrl:")
+    print(result.responseUrl)
