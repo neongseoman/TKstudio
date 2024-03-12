5baa3620cbef3ce8588eb9929ea50e85c1f96f90
고정원 <7704wjddnjs@naver.com>
2024-03-12 15:19:13 +0900

    [BE] Feat: 리프레시 토큰으로 로그인 시 토큰 재발급

--- backend/src/main/java/com/ssafy/gallery/auth/jwt/util/JwtUtil.java
+++ backend/src/main/java/com/ssafy/gallery/auth/jwt/util/JwtUtil.java
@@ -6,8 +6,10 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -23,14 +25,24 @@ import java.util.UUID;
 public class JwtUtil {
     private final LoginTokenRepository loginTokenRepository;
 
-    public static String createJwt(String secretKey, Long expiredMs) {
+    @Value("${jwt.access.expiration}")
+    private long accessExpiration;
+    @Value("${jwt.refresh.expiration}")
+    private long refreshExpiration;
+
+    public String createToken(String type) {
+        String secretKey = UUID.randomUUID().toString() + UUID.randomUUID().toString();
+        secretKey = secretKey.replace("-", "");
+        long expiration = type.equals("access") ? accessExpiration : refreshExpiration;
+        log.info("{} 토큰 만들기, secretKey: {}", type, secretKey);
+
         Claims claims = Jwts.claims();
         claims.put("jti", UUID.randomUUID());
 
         return Jwts.builder()
                 .setClaims(claims) // 만들어 놓은 claim을 넣는 것
                 .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + expiredMs))
+                .setExpiration(new Date(System.currentTimeMillis() + expiration))
                 .signWith(SignatureAlgorithm.HS256, secretKey)
                 .compact();
     }
@@ -39,21 +51,43 @@ public class JwtUtil {
         return request.getHeader(HttpHeaders.AUTHORIZATION);
     }
 
-    public UsernamePasswordAuthenticationToken getAuthentication(String token) {
+    public void saveTokens(String accessToken, String refreshToken, int userId) {
+        log.info("Redis에 토큰 저장 : {}, {}", accessToken, refreshToken);
+        LoginTokenDto accessTokenDto = new LoginTokenDto(accessToken, userId, null, accessExpiration);
+        LoginTokenDto refreshTokenDto = new LoginTokenDto(refreshToken, userId, accessToken, refreshExpiration);
+        loginTokenRepository.save(accessTokenDto);
+        loginTokenRepository.save(refreshTokenDto);
+    }
+
+    public UsernamePasswordAuthenticationToken getAuthentication(HttpServletRequest request, HttpServletResponse response, String token) {
         token = token.split(" ")[1];
-        log.info("token: {}", token);
+        log.info("getAuthentication: {}", token);
 
         if (loginTokenRepository.findById(token).isPresent()) {
             LoginTokenDto loginTokenDto = loginTokenRepository.findById(token).get();
             log.info("로그인한 유저의 정보: {}", loginTokenDto);
 
-            // accessToken이 아직 존재하면 비정상적인 접근 -> 다시 로그인 시키기
             String accessToken = loginTokenDto.getAccessToken();
             if (accessToken != null) {
                 if (loginTokenRepository.findById(accessToken).isPresent()) {
+                    // accessToken이 아직 유효하면 비정상적인 접근 -> 다시 로그인 시키기
+                    log.info("로그아웃: 엑세스 토큰, 리프레시 토큰 삭제");
+
                     loginTokenRepository.deleteById(accessToken);
                     loginTokenRepository.deleteById(token);
                     return null;
+                } else {
+                    // accessToken이 만료되었으면 둘 다 재발급
+                    log.info("리프레시 토큰 로그인: 토큰 재발급");
+                    loginTokenRepository.deleteById(token);
+
+                    accessToken = createToken("access");
+                    String refreshToken = createToken("refresh");
+                    saveTokens(accessToken, refreshToken, loginTokenDto.getUserId());
+
+                    response.setHeader("accessToken", accessToken);
+                    response.setHeader("refreshToken", refreshToken);
+                    request.setAttribute("refreshToken", refreshToken);
                 }
             }
 
