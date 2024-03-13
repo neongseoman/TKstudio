5489fa0e6d0dd3bc5f6829e49e4983770876547c
이운재 <hil00077@khu.ac.kr>
2024-03-13 14:42:06 +0900

    [FE] feat: /result skeleton

--- frontend/src/app/(main)/create/result/page.tsx
+++ frontend/src/app/(main)/create/result/page.tsx
@@ -1,18 +1,81 @@
-// /create에서 만든 AI서버로 요청한 사진을 바당서 게시
 'use client'
-import Button from "@/components/Button"
+import Button from '@/components/Button'
+import ImageWrapper from '@/components/ImageWrapper'
+import { useRouter } from 'next/navigation'
+import styled from 'styled-components'
 
-const Result = function() {
+const MainWrapper = styled.main`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  margin-top: 20px;
+`
+
+const Result = function () {
+  // /create 에서 딸깍사진을 받아야함
+  const router = useRouter()
+  const handleDelete = async () => {
+    const accessToken = localStorage.getItem('accessToken')
+
+    if (accessToken !== null) {
+      const requestOptions = {
+        method: 'DELETE',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: accessToken,
+        },
+      }
+
+      try {
+        const response = await fetch('백엔드 URL', requestOptions)
+        if (response.ok) {
+          console.log('삭제 성공')
+          router.push('/create')
+          // 필요한 경우 추가적인 처리
+        } else {
+          console.error('삭제 실패')
+          // 필요한 경우 실패 시 처리
+        }
+      } catch (error) {
+        console.error('삭제 요청 중 오류 발생', error)
+        // 필요한 경우 오류 처리.
+      }
+    } else {
+      console.error('Access token이 없습니다.')
+      alert('로그인이 필요합니다')
+      router.push('/login')
+      // 필요한 경우 access token이 없을 때의 처리.
+    }
+  }
   return (
-    <div>
+    <MainWrapper>
+      <ImageWrapper
+        // 임시 사진 - create에서 받아온 사진을 게시할 예정
+        src="https://blog.kakaocdn.net/dn/TJL7n/btr1Wmgwtgu/3RJ7dheqh6T8bjumKp5LzK/img.png"
+        alt="AI완성 사진"
+        $width="300px"
+        origin={true}
+      />
 
-      <img src="" alt="AI완성 사진" />
       <div>
-        <Button>확인</Button>
-        <Button>삭제</Button>
+        <Button
+          onClick={() => router.push('/create')}
+          $width="80px"
+          $margin="20px"
+        >
+          확인
+        </Button>
+        <Button
+          onClick={handleDelete}
+          $backgroundColor="red"
+          $width="80px"
+          $margin="20px"
+        >
+          삭제
+        </Button>
       </div>
-    
-    </div>
+    </MainWrapper>
   )
 }
 
