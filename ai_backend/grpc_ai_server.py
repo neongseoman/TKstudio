e1061be793e3c3776c3066b315818329e62f78a7
정진영 <wlsehfdl95@naver.com>
2024-03-13 12:41:37 +0900

    [AI] Feat: 유효성 검사 기능 추가
    Former-commit-id: 33b32d35838b6bf3c321d34f2d2ae1f291bba4dc

--- ai_backend/grpc_ai_server.py
+++ ai_backend/grpc_ai_server.py
@@ -34,8 +34,8 @@ class CreateImageService(pb2_grpc.CreateImageServicer):
             # Bytes to ndarray
             image_array = np.frombuffer(original_image_bytes, dtype=np.uint8)
             original_image = cv2.imdecode(image_array, cv2.IMREAD_COLOR)
-            # options = request.options  # 이후 옵션에 따라 템플릿사진 선택
-            # gender = options.gender
+            options = request.options  # 이후 옵션에 따라 템플릿사진 선택
+            sex = options.SEX
             # bg = options.background
             # suit = options.suit
             # hair = options.hair
@@ -43,8 +43,10 @@ class CreateImageService(pb2_grpc.CreateImageServicer):
             # Detect face from bg img
 
             # Img dir
-            gender = "male"  # temp gender
-            bg_0_img_path = f"./src/{gender}_bg_0.jpg"  # f-string 사용해서 옵션에 따라 bg image 변경
+            # sex = "FEMALE"  # sex test code
+            bg_0_img_path = (
+                f"./src/{sex}_bg_0.jpg"  # f-string 사용해서 옵션에 따라 bg image 변경
+            )
             bg_0_img = plt.imread(bg_0_img_path)
             bg_faces = faceswap_app.get(bg_0_img)
             bg_face = bg_faces[0]
@@ -52,31 +54,33 @@ class CreateImageService(pb2_grpc.CreateImageServicer):
             try:
                 # Detect face from input img
                 faces = faceswap_app.get(original_image)
-                source_face = faces[0]
 
+                # Many faces in input img
+                if len(faces) > 1:
+                    print("There are more than one face in the input image")
+
+                    return_value = {
+                        "processedImage": b"",  # 비어있는 bytes 데이터
+                        "responseUrl": {},  # 비어있는 responseUrl
+                        "result": pb2.ImageProcessingResult.MANY_FACE,
+                    }
+
+                    return pb2.ProcessedImageInfo(**return_value)
+
+            # Handle error: No face detected or other issues
             except Exception as err:
-                # Handle error: No face detected or other issues
                 print(f"Error detecting face in original image: {err}")
 
-                # context.set_code(grpc.StatusCode.UNAVAILABLE)
-                # context.set_details("Failed to detect face in input image.")
+                return_value = {
+                    "processedImage": b"",  # 비어있는 bytes 데이터
+                    "responseUrl": {},  # 비어있는 responseUrl
+                    "result": pb2.ImageProcessingResult.NO_FACE,  # NO_FACE 결과 설정
+                }
 
-                # response_url = {
-                #     "originalImageUrl": "",
-                #     "processedImageUrl": "",
-                #     "thumbnailImageUrl": "",
-                # }
-
-                # result = {
-                #     "processedImage": b"0",
-                #     "responseUrl": response_url,
-                #     "status": False,
-                #     "errorMessage": err,
-                # }
-                # return pb2.ProcessedImageInfo(**result)
-                return None
+                return pb2.ProcessedImageInfo(**return_value)
 
             # Swap face from bg img to input img
+            source_face = faces[0]
             processed_image = bg_0_img.copy()
             processed_image = swapper.get(
                 bg_0_img, bg_face, source_face, paste_back=True
@@ -85,6 +89,7 @@ class CreateImageService(pb2_grpc.CreateImageServicer):
             # 이미지 확인용
             plt.imshow(processed_image)
             plt.show()
+
             processed_image = cv2.cvtColor(
                 processed_image, cv2.COLOR_BGR2RGB
             )  # BGR -> RGB 채널 변경
@@ -98,38 +103,30 @@ class CreateImageService(pb2_grpc.CreateImageServicer):
             # Add codes to save images in S3 server
 
             response_url = {
-                "originalImageUrl": "original_image_url_sample",
-                "processedImageUrl": "processed_image_url_sample",
-                "thumbnailImageUrl": "thumbnail_image_url_sample",
+                # "originalImageUrl": "original_image_url_sample",
+                # "processedImageUrl": "processed_image_url_sample",
+                # "thumbnailImageUrl": "thumbnail_image_url_sample",
             }
 
-            result = {
+            return_value = {
                 "processedImage": processed_image,
                 "responseUrl": response_url,
-                # "status": True,
-                # "errorMessage": None,
+                "result": pb2.ImageProcessingResult.SUCCESS,
             }
 
-            return pb2.ProcessedImageInfo(**result)
+            return pb2.ProcessedImageInfo(**return_value)
 
         except Exception as err:
             # Handle error: Failed to process image
             print(f"Error processing image: {err}")
 
-            # response_url = {
-            #     "originalImageUrl": "",
-            #     "processedImageUrl": "",
-            #     "thumbnailImageUrl": "",
-            # }
+            return_value = {
+                "processedImage": b"",  # 비어있는 bytes 데이터
+                "responseUrl": {},  # 비어있는 responseUrl
+                "result": pb2.ImageProcessingResult.NO_FACE,  # NO_FACE 결과 설정
+            }
 
-            # result = {
-            #     "processedImage": b"0",
-            #     "responseUrl": response_url,
-            #     "status": False,
-            #     "errorMessage": err,
-            # }
-            # return pb2.ProcessedImageInfo(**result)
-            return None
+            return pb2.ProcessedImageInfo(**return_value)
 
 
 def serve():
