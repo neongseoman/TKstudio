227ed2a7633e27a6dea52d9745e831739f3742f3
박근수 <rmstn8426@gmail.com>
2024-03-18 13:50:11 +0900

    [BE] Feat: create image api return urls

--- backend/src/main/java/com/ssafy/gallery/image/controller/ImageController.java
+++ backend/src/main/java/com/ssafy/gallery/image/controller/ImageController.java
@@ -1,24 +1,23 @@
 package com.ssafy.gallery.image.controller;
 
+import com.ssafy.gallery.image.model.CreateImage;
 import com.ssafy.gallery.image.model.ImageOption;
+import com.ssafy.gallery.image.model.SEX;
 import com.ssafy.gallery.image.service.ImageService;
+import com.ssafy.pjt.grpc.Image;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.springframework.core.io.ByteArrayResource;
-import org.springframework.core.io.Resource;
-import org.springframework.core.io.ResourceLoader;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import javax.imageio.ImageIO;
-import java.io.IOException;
 import java.util.List;
+import java.util.Map;
 
 @Tag(name = "이미지", description = "Image API")
 @Log4j2
@@ -31,18 +30,21 @@ public class ImageController {
 
     // Login한 사용자의 UserId값이 있어야 JPA로 DB에 데이터 넘길 수 있음.
     @PostMapping("/create")
-    public ResponseEntity<ByteArrayResource> createImage(
+    public ResponseEntity<CreateImage> createImage(
             @RequestParam(value = "originalImage") MultipartFile originalImage,
             @RequestParam(value = "background") String background,
             @RequestParam(value = "suit") String suit,
-            @RequestParam(value = "hair") String hair
+            @RequestParam(value = "hair") String hair,
+            @RequestParam(value = "sex") Image.SEX sex
+
     ) throws Exception {
 
         try{
+//            System.out.println("com");
 
-            ByteArrayResource response = imageService.sendImage(originalImage,new ImageOption(background,suit,hair));
+            CreateImage response = imageService.sendImage(originalImage,new ImageOption(background,suit,hair,sex));
             return ResponseEntity.ok()
-                    .header(HttpHeaders.CONTENT_TYPE, MediaType.IMAGE_JPEG_VALUE)
+                    .header(HttpHeaders.CONTENT_TYPE,MediaType.APPLICATION_JSON_VALUE)
                     .body(response);
 
         } catch (Exception e){
