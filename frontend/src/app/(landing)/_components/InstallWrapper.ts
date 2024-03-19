3a152080c9f6d8d1cabb840e84f3173b2f2bdbd2
이지원 <leeji703@naver.com>
2024-03-19 17:30:29 +0900

    [FE] Feat: 랜딩페이지

---
+++ frontend/src/app/(landing)/_components/InstallWrapper.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+const InstallWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 20rem;
+  align-items: center;
+  padding-top: 0.75rem;
+`
+
+export default InstallWrapper
--- frontend/src/app/(landing)/page.tsx
+++ frontend/src/app/(landing)/page.tsx
@@ -2,19 +2,28 @@
 
 import { useState, useEffect } from 'react'
 import DownloadIcon from '@@/assets/icons/download.svg'
-import { useRouter } from 'next/navigation'
+import Button from '@/components/Button'
+import InstallWrapper from './_components/InstallWrapper'
+import XWrapper from './_components/XWrapper'
+import SlideupModal from '@/components/SlideupModal'
+import LogoSmall from '@@/assets/icons/logo-small.svg'
+import XIcon from '@@/assets/icons/x.svg'
+import InstallHeader from './_components/InstallHeader'
+import InstallLogo from './_components/InstallLogo'
 
 function LandingPage() {
-  const router = useRouter()
+  // const router = useRouter()
   const [device, setDevice] = useState<string | null>(null)
   const [deferredPrompt, setDeferredPrompt] = useState<any>(null)
+  const [seen, setSeen] = useState<boolean>(false)
+  const [isClose, setIsClose] = useState<boolean>(true)
 
-  function handleBeforeInstallPrompt(event: any) {
+  const handleBeforeInstallPrompt = (event: any) => {
     event.preventDefault()
     setDeferredPrompt(event)
   }
 
-  function handleInstallClick() {
+  const handleInstallClick = () => {
     if (deferredPrompt) {
       deferredPrompt.prompt()
 
@@ -24,17 +33,30 @@ function LandingPage() {
     }
   }
 
-  function renderMessage() {
+  const handleClose = async () => {
+    setIsClose(true)
+    await new Promise((resolve) => setTimeout(resolve, 300))
+    setSeen(false)
+  }
+
+  const renderMessage = () => {
     if (device === 'web') {
       return <p>모바일 환경에서 접속해 주세요</p>
     } else if (device === 'android') {
-      return (
-        <p>
-          어플리케이션을 이용해 주세요
-          {deferredPrompt && (
-            <button onClick={handleInstallClick}>앱 설치</button>
-          )}
-        </p>
+      return deferredPrompt ? (
+        <Button
+          $fontSize='1.5rem'
+          $padding='1rem'
+          $borderRadius='1rem'
+          onClick={() => {
+            handleInstallClick()
+            handleClose()
+          }}
+        >
+          앱 다운로드
+        </Button>
+      ) : (
+        <p>앱을 이용해 주세요</p>
       )
     } else if (device === 'iphone') {
       return (
@@ -49,16 +71,7 @@ function LandingPage() {
   useEffect
   useEffect(() => {
     window.addEventListener('beforeinstallprompt', handleBeforeInstallPrompt)
-    console.log(window)
-    return () => {
-      window.removeEventListener(
-        'beforeinstallprompt',
-        handleBeforeInstallPrompt,
-      )
-    }
-  }, [])
 
-  useEffect(() => {
     const agent = navigator.userAgent
     let tempDevice = 'web'
 
@@ -69,14 +82,41 @@ function LandingPage() {
     }
 
     setDevice(tempDevice)
+    setIsClose(false)
+    setSeen(true)
 
-    router.push(`/install/${tempDevice}`, {scroll: false})
-  }, [router])
+    return () => {
+      window.removeEventListener(
+        'beforeinstallprompt',
+        handleBeforeInstallPrompt,
+      )
+    }
+  }, [])
 
   return (
     <>
-      <h1>랜딩페이지</h1>
-      {renderMessage()}
+      {seen && (
+        <SlideupModal
+          isClose={isClose}
+          height="20rem"
+          handleClose={handleClose}
+        >
+          <InstallWrapper>
+            <InstallHeader>
+              <XWrapper onClick={() => {
+                handleClose()
+              }}>
+                <XIcon />
+              </XWrapper>
+              <InstallLogo>
+                <LogoSmall />
+              </InstallLogo>
+            </InstallHeader>
+            <h2 style={{width: '90%', marginBottom: '3rem'}}>앱을 사용하여 최적의 환경에서 이용해 보세요</h2>
+            {renderMessage()}
+          </InstallWrapper>
+        </SlideupModal>
+      )}
     </>
   )
 }
