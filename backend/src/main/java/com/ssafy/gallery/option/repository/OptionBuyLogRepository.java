8c4d103a5d5d8ba80c6f07ca8714f68d45d9168d
고정원 <7704wjddnjs@naver.com>
2024-03-20 14:16:53 +0900

    [BE] Feat: 옵션 구매여부 Redis에 캐싱, 리스트 반환 시 구매여부 포함하여 반환

--- backend/src/main/java/com/ssafy/gallery/option/repository/OptionBuyLogRepository.java
+++ backend/src/main/java/com/ssafy/gallery/option/repository/OptionBuyLogRepository.java
@@ -3,5 +3,8 @@ package com.ssafy.gallery.option.repository;
 import com.ssafy.gallery.option.model.OptionBuyLog;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface OptionBuyLogRepository extends JpaRepository<OptionBuyLog, Integer> {
+    List<OptionBuyLog> findAllByUserId(int userId);
 }
