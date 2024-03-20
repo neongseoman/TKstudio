4a8043a9a6e934e6ac91f6c7f111b76a56a8fbd6
이지원 <leeji703@naver.com>
2024-03-20 14:28:14 +0900

    [FE] Design: 랜딩페이지 css

--- frontend/src/app/(landing)/_components/InstallHeader.ts
+++ frontend/src/app/(landing)/_components/InstallHeader.ts
@@ -3,7 +3,7 @@ import styled from 'styled-components'
 const InstallHeader = styled.div`
   display: grid;
   grid-template-columns: 3rem 1fr 3rem;
-  width: 90%;
+  width: 97%;
   place-items: center;
   margin-bottom: 1rem;
 `
