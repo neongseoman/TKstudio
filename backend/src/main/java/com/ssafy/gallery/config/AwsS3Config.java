c77d56fb2008f7c01de20d279a0aaf62b04ca068
박근수 <rmstn8426@gmail.com>
2024-03-21 16:08:11 +0900

    [BE] Feat : add aws file

--- backend/src/main/java/com/ssafy/gallery/config/AwsS3Config.java
+++ backend/src/main/java/com/ssafy/gallery/config/AwsS3Config.java
@@ -1,11 +1,8 @@
 package com.ssafy.gallery.config;
 
-import com.amazonaws.auth.AWSCredentials;
-import com.amazonaws.auth.AWSCredentialsProvider;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -37,4 +34,5 @@ public class AwsS3Config {
 
         return s3Builder;
     }
+
 }
