6996ae762862ab156d52b776d65ed26053a01a59
고정원 <7704wjddnjs@naver.com>
2024-03-22 17:13:21 +0900

    [BE] Feat: Naver 로그인 구현

--- backend/src/main/java/com/ssafy/gallery/config/HttpInterfaceConfig.java
+++ backend/src/main/java/com/ssafy/gallery/config/HttpInterfaceConfig.java
@@ -1,6 +1,7 @@
 package com.ssafy.gallery.config;
 
 import com.ssafy.gallery.oauth.client.KakaoApiClient;
+import com.ssafy.gallery.oauth.client.NaverApiClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -13,6 +14,11 @@ public class HttpInterfaceConfig {
     @Bean
     public KakaoApiClient kakaoApiClient() {
         return createHttpInterface(KakaoApiClient.class);
+    }
+
+    @Bean
+    public NaverApiClient naverApiClient() {
+        return createHttpInterface(NaverApiClient.class);
     }
 
     private <T> T createHttpInterface(Class<T> clazz) {
