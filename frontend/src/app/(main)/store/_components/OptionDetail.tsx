49817498eea01577c2ecadd3def559d971025bfc
Kim Ji Yong <jykim00324@gmail.com>
2024-03-25 12:49:18 +0900

    [FE] Chore: Prevent mouse right click

--- frontend/src/app/(main)/store/_components/OptionDetail.tsx
+++ frontend/src/app/(main)/store/_components/OptionDetail.tsx
@@ -47,7 +47,11 @@ const OptionDetailTextWrapper = styled.div<PurchasedOptionStyleProp>`
 
 function OptionDetail(props: Option) {
   return (
-    <OptionDetailWrapper>
+    <OptionDetailWrapper
+      onContextMenu={(e) => {
+        event?.preventDefault()
+      }}
+    >
       <ImageWrapper src={props.optionS3Url} alt="optionImg" />
       <OptionDetailTextWrapper $purchased={props.purchased}>
         <div>{props.optionName}</div>
