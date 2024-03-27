09c1e1ff52c8be71e323dd7b567964d1d9065ac4
고정원 <7704wjddnjs@naver.com>
2024-03-27 14:23:13 +0900

    [BE] Feat: 옵션스토어 API 예외처리

--- backend/src/main/java/com/ssafy/gallery/common/exception/ApiExceptionAdvice.java
+++ backend/src/main/java/com/ssafy/gallery/common/exception/ApiExceptionAdvice.java
@@ -1,21 +1,32 @@
 package com.ssafy.gallery.common.exception;
 
 import com.ssafy.gallery.common.response.ApiResponse;
-import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.dao.DataAccessException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import static com.ssafy.gallery.common.exception.CommonExceptionEnum.DATA_ACCESS_ERROR;
 
 @RestControllerAdvice
 public class ApiExceptionAdvice {
 
     @ExceptionHandler(ApiException.class)
     @ResponseStatus
-    public ResponseEntity<ApiResponse<?>> handleApiException(HttpServletRequest request, ApiException e) {
+    public ResponseEntity<ApiResponse<?>> handleApiException(ApiException e) {
 
         return ResponseEntity
                 .status(e.getStatus())
                 .body(ApiResponse.error(e.getMessage()));
+    }
+
+    @ExceptionHandler(DataAccessException.class)
+    @ResponseStatus
+    public ResponseEntity<ApiResponse<?>> handleDataException() {
+
+        return ResponseEntity
+                .status(DATA_ACCESS_ERROR.getStatus())
+                .body(ApiResponse.error(DATA_ACCESS_ERROR.getMessage()));
     }
 }
