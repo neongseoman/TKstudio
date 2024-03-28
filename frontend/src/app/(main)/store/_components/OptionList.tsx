84a51ca9f37f1746b2f915898b773a7ac32216b0
Kim Ji Yong <jykim00324@gmail.com>
2024-03-28 14:46:53 +0900

    [FE] Feat: Add fetch token refresh

--- frontend/src/app/(main)/store/_components/OptionList.tsx
+++ frontend/src/app/(main)/store/_components/OptionList.tsx
@@ -2,6 +2,8 @@ import styled from 'styled-components'
 import OptionDetail from './OptionDetail'
 import { useEffect, useState } from 'react'
 import { GenderCategory } from '../page'
+import { useRouter } from 'next/navigation'
+import { fetchDataWithAuthorization } from '@/utils/api'
 
 const OptionListContainer = styled.div`
   display: flex;
@@ -31,18 +33,91 @@ function OptionList({ categorySort, showMine }: OptionListProp) {
 
   async function getOptionList() {
     const accessToken = localStorage.getItem('accessToken') as string
-    const optionListResponse = await fetch(
-      process.env.NEXT_PUBLIC_BACK_URL + '/api/v1/option/list',
-      {
-        headers: { Authorization: accessToken },
-      },
+    const refreshToken = localStorage.getItem('refreshToken') as string
+    // let optionListResponse = await fetch(
+    //   process.env.NEXT_PUBLIC_BACK_URL + '/api/v1/option/list',
+    //   {
+    //     headers: { Authorization: accessToken },
+    //   },
+    // )
+
+    // if (optionListResponse.status == 401) {
+    //   optionListResponse = await fetch(
+    //     process.env.NEXT_PUBLIC_BACK_URL + '/api/v1/option/list',
+    //     {
+    //       headers: { Authorization: refreshToken },
+    //     },
+    //   )
+    //   if (optionListResponse.status == 401) {
+    //     localStorage.removeItem('accessToken')
+    //     localStorage.removeItem('refreshToken')
+    //     alert('로그인이 만료되었습니다.')
+    //     router.push('/login')
+    //     return
+    //   }
+    //   alert('refreshToken Update')
+    //   localStorage.setItem(
+    //     'accessToken',
+    //     'Bearer ' + optionListResponse.headers.get('accessToken')!,
+    //   )
+    //   localStorage.setItem(
+    //     'refreshToken',
+    //     'Bearer ' + optionListResponse.headers.get('refreshToken')!,
+    //   )
+    // }
+
+    // async function fetchDataWithAuthorization(
+    //   url: string,
+    //   accessToken: string,
+    //   refreshToken: string,
+    //   requestOptions: any = {},
+    // ) {
+    //   let response = await fetch(url, {
+    //     ...requestOptions,
+    //     headers: {
+    //       Authorization: accessToken,
+    //       ...(requestOptions.headers || {}),
+    //     },
+    //   })
+
+    //   if (response.status === 401) {
+    //     response = await fetch(url, {
+    //       ...requestOptions,
+    //       headers: {
+    //         Authorization: refreshToken,
+    //         ...(requestOptions.headers || {}),
+    //       },
+    //     })
+
+    //     if (response.status === 401) {
+    //       localStorage.removeItem('accessToken')
+    //       localStorage.removeItem('refreshToken')
+    //       alert('로그인이 만료되었습니다.')
+    //       router.push('/login')
+    //       return null
+    //     }
+
+    //     alert('refreshToken Update')
+    //     const newAccessToken = response.headers.get('accessToken')
+    //     const newRefreshToken = response.headers.get('refreshToken')
+    //     localStorage.setItem('accessToken', 'Bearer ' + newAccessToken)
+    //     localStorage.setItem('refreshToken', 'Bearer ' + newRefreshToken)
+    //   }
+
+    //   return response
+    // }
+    const url = process.env.NEXT_PUBLIC_BACK_URL + '/api/v1/option/list'
+
+    const requestOptions = {}
+
+    const optionListResponse = await fetchDataWithAuthorization(
+      url,
+      accessToken,
+      refreshToken,
+      requestOptions,
     )
 
-    if (!optionListResponse.ok) {
-      throw new Error('옵션 리스트 불러오기 실패')
-    }
-
-    const optionListJson = await optionListResponse.json()
+    const optionListJson = await optionListResponse?.json()
 
     const optionList: Option[] = optionListJson.data
 
