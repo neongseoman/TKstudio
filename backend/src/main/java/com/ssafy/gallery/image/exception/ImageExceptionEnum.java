3fc35e1a82a2d11c847faa3864d41a619a643c21
고정원 <7704wjddnjs@naver.com>
2024-03-29 15:42:12 +0900

    [BE] Fix: 이미지 생성 서비스 log 제거

--- backend/src/main/java/com/ssafy/gallery/image/exception/ImageExceptionEnum.java
+++ backend/src/main/java/com/ssafy/gallery/image/exception/ImageExceptionEnum.java
@@ -8,10 +8,10 @@ import org.springframework.http.HttpStatus;
 @Getter
 @AllArgsConstructor
 public enum ImageExceptionEnum implements ExceptionEnum {
-    NO_FACE(HttpStatus.NOT_ACCEPTABLE, 900, "얼굴이 없습니다."),
-    MANY_FACE(HttpStatus.NOT_EXTENDED, 901, "탐지된 얼굴이 2개 이상입니다."),
-    GRPC_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, 500,"GRPC ERROR"),
-    NO_INFO(HttpStatus.BAD_REQUEST,400,"어떤 에러.");
+    NO_FACE(HttpStatus.BAD_REQUEST, 400, "얼굴이 없습니다."),
+    MANY_FACE(HttpStatus.BAD_REQUEST, 400, "탐지된 얼굴이 2개 이상입니다."),
+    GRPC_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, 500, "GRPC ERROR"),
+    NO_INFO(HttpStatus.BAD_REQUEST, 400, "어떤 에러.");
 
     private final HttpStatus status;
     private final int code;
