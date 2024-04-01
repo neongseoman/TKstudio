229a6b24c5661010adcebf334561ed9ab8389a7f
고정원 <7704wjddnjs@naver.com>
2024-04-01 16:20:41 +0900

    [BE] Feat: 옵션 리스트 이미지 api 추가(URL이 아닌 Resource 반환)

--- backend/src/main/java/com/ssafy/gallery/option/repository/OptionCategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ssafy.gallery.option.repository;
-
-import com.ssafy.gallery.option.model.OptionCategory;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OptionCategoryRepository extends JpaRepository<OptionCategory, Integer> {
-}
