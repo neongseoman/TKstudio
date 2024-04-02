34dd51d5382cf0e99cd78378dd27bd3783622942
Jiwon Lee <leeji7031@gmail.com>
2024-04-02 11:36:22 +0900

    [FE] Design: 해더 디자인, 캐로셀 위치 변경

--- frontend/src/app/(main)/home/_component/_atom/CarouselMain.ts
+++ frontend/src/app/(main)/home/_component/_atom/CarouselMain.ts
@@ -2,13 +2,14 @@ import styled from 'styled-components'
 import { Gray } from '@@/assets/styles/pallete'
 
 const CarouselMain = styled.div`
-  width: 70vw;
+  width: 80vw;
   position: relative;
   overflow: hidden;
   user-select: none;
   margin: 5vw 0;
   box-shadow: 4px 4px 10px 3px ${Gray};
   box-sizing: border-box;
+  border-radius: 20px;
 `
 
 export default CarouselMain
