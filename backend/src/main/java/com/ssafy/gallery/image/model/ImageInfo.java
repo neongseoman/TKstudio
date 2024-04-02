784510fbf0512c13149dc4fe62e4782e0d19b2da
고정원 <7704wjddnjs@naver.com>
2024-04-02 10:14:41 +0900

    [BE] Fix: 백엔드 코드 롤백

--- backend/src/main/java/com/ssafy/gallery/image/model/ImageInfo.java
+++ backend/src/main/java/com/ssafy/gallery/image/model/ImageInfo.java
@@ -1,5 +1,6 @@
 package com.ssafy.gallery.image.model;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import com.ssafy.gallery.option.model.OptionStore;
 import jakarta.persistence.*;
 import lombok.*;
@@ -7,6 +8,8 @@ import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
@@ -38,16 +41,18 @@ public class ImageInfo {
 
     @Column
     private String optionName;
-    @Column
-    private int optionId;
 
-    public ImageInfo(int userId, String originalImageUrl, String thumbnailImageUrl, String processedImageUrl, OptionStore optionStore) {
+    @ManyToOne
+    @JoinColumn(name = "optionId")
+    private OptionStore optionStore;
+
+    public ImageInfo(int userId, String originalImageUrl, String thumbnailImageUrl, String processedImageUrl,OptionStore optionStore) {
         this.userId = userId;
         try {
-            this.thumbnailImageUrl = thumbnailImageUrl.substring(thumbnailImageUrl.indexOf("processedImages/"));
+            this.thumbnailImageUrl = thumbnailImageUrl.substring(thumbnailImageUrl.indexOf("thumbnailImages/"));
             this.originalImageUrl = originalImageUrl.substring(originalImageUrl.indexOf("originalImages/"));
             this.processedImageUrl = processedImageUrl.substring(processedImageUrl.indexOf("processedImages/"));
-            this.optionId = optionStore.getOptionId();
+            this.optionStore = optionStore;
             this.optionName = optionStore.getOptionName();
         } catch (IndexOutOfBoundsException e) {
             throw new IllegalArgumentException("Invalid image URL format", e);
